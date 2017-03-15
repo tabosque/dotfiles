01094218b5a6f8e77f28bd736fe34425d12882bb
tabosque <devotionidea@gmail.com>
2017-03-15 15:40:17 +0900

    Sublime Textの設定を追加

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -si $DOTFILES_DIR/.bashrc $HOME/.bashrc
 ln -si $DOTFILES_DIR/.bash_profile $HOME/.bash_profile
 ln -si $DOTFILES_DIR/.vimrc $HOME/.vimrc
 ln -si $DOTFILES_DIR/.gemrc $HOME/.gemrc
+ln -si $DOTFILES_DIR/Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
