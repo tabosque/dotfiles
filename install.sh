a9251e39c5facc7346bbb0c6bb23f37613d188d8
tabosque <devotionidea@gmail.com>
2017-04-26 15:01:12 +0900

    unaliasとsourceを削除

--- install.sh
+++ install.sh
@@ -9,19 +9,3 @@ ln -si $DOTFILES_DIR/.gemrc $HOME/.gemrc
 if [ -d $HOME/Library/Application\ Support ]; then
   ln -si $DOTFILES_DIR/Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 fi
-
-while true; do
-  read -p 'Do you reload dotfiles? [Y/n]' Answer
-  case $Answer in
-    '' | [Yy]* )
-      source ~/.bash_profile
-      echo 'Reloaded .bash_profile'
-      break;
-      ;;
-    [Nn]* )
-      break;
-      ;;
-    * )
-      echo Please answer YES or NO.
-  esac
-done;
